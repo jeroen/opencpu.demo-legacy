ad5962f1fc94beadb3c015d8ed20bafc09772f25
Jeroen <jeroen@jeroen-ubuntu.(none)>
2012-05-05 15:59:54 -0700

    some cleanup

--- R/gitstats.R
+++ R/gitstats.R
@@ -8,11 +8,9 @@
 #' @export
 gitstats <- function (id = "hadley", type = "user") 
 {
-    require(RCurl)
-    require(RJSONIO)
-    require(ggplot2)
-    require(reshape2)
-    require(plyr)
+	#ggplot is easier to load.
+	require(ggplot2);
+	
     if (type == "user") {
         url = "https://api.github.com/users/"
     }
@@ -21,25 +19,27 @@ gitstats <- function (id = "hadley", type = "user")
     }
     else stop("parameter 'type' has to be either 'user' or 'org' ")
     url2 <- paste(url, id, "/repos?per_page=100", sep = "")
-    xx <- getURL(url2)
-    tt <- fromJSON(xx)
+    xx <- RCurl::getURL(url2)
+    tt <- RJSONIO::fromJSON(xx)
     if (!length(tt) == 1) {
         tt <- tt
     }
     else {
         stop("user or organization not found - search GitHub? - https://github.com/")
     }
-    out <- ldply(tt, function(x) t(c(x$name, x$forks, x$watchers)))
+    out <- plyr::ldply(tt, function(x) t(c(x$name, x$forks, x$watchers)))
     names(out) <- c("Repo", "Forks", "Watchers")
     out$Forks <- as.integer(out$Forks)
     out$Watchers <- as.integer(out$Watcher)
-    out2 <- melt(out, id = 1)
+    out2 <- reshape2::melt(out, id = 1)
     out2$value <- as.numeric(out2$value)
     out2$Repo <- as.factor(out2$Repo)
     repoorder <- unique(out2[order(out2$value, decreasing = FALSE), 
         ][, 1])
     out2$Repo <- factor(out2$Repo, levels = repoorder)
-    ggplot(out2, aes(Repo, value)) + geom_bar() + coord_flip() + 
-        facet_wrap(~variable) + theme_bw(base_size = 18)
+	myplot <- ggplot(out2, aes(Repo, value)) + geom_bar() + coord_flip() + 
+			facet_wrap(~variable) + theme_bw(base_size = 18)
+	print(myplot)
+	invisible();
 }
 
