9d2f5a5afbeac318b605fc2ff7353e4985ec450b
Scott Chamberlain <myrmecocystus@gmail.com>
2012-05-12 15:17:00 -0500

    with altmetrics fxn can now put in multiple users for both digg and github

--- R/altmetrics.R
+++ R/altmetrics.R
@@ -7,23 +7,26 @@
 #' @param gittype GitHub type, 'user' or 'org'
 #' @author Scott Chamberlain
 #' @export
-altmetrics <- function (diggname = "cboettig", gitid = 'cboettig', 
-      gittype = "user") 
+altmetrics <- function(diggname = list("cboettig","lo0ol"), 
+                       gitid = list("cboettig", "holman"), gittype = "user") 
 {
-    #ggplot is easier to load.
-    require(ggplot2); require(RCurl); require(RJSONIO); require(plyr)
-	
-    diggs <- digg_user(usernames = diggname)
-    gits <- git_wf(id=gitid, type=gittype)
-    diggs_ <- data.frame(names(diggs), diggs[[1]])
-    names(diggs_) <- c("metric","value")
-    gits_ <- data.frame(names(gits), gits)
-    names(gits_) <- c("metric","value")
-    df <- rbind(diggs_, gits_)
-    
-    myplot <- ggplot(df, aes(metric, value)) + geom_bar() + theme_bw(base_size = 18)
-    print(myplot)
-    invisible();
+  #ggplot is easier to load.
+  require(ggplot2); require(RCurl); require(RJSONIO); require(plyr)
+  
+  digout <- laply(diggname, digg_user)
+  diggs_ <- data.frame(ldply(gitid), digout, rep("diggs", length(gitid)))
+  gitout <- laply(gitid, git_wf)
+  gits_ <- data.frame(ldply(gitid), ldply(gitout), 
+        rep(c("gitforks","gitwatchers"), each=length(gitid)))
+  names(diggs_) <- c("user","value","metric")
+  names(gits_) <- c("user","value","metric")
+  df <- rbind(diggs_, gits_)
+  myplot <- ggplot(df, aes(metric, value, fill=user)) + 
+    geom_bar(position="dodge") +
+    theme_bw(base_size = 18)
+  message("names default to Github user names")
+  print(myplot)
+  invisible();
 }
 
 #' Get Digg metrics on a user.
@@ -47,7 +50,7 @@ digg_user <- function(usernames = NA, userids = NA,
     args$usernames <- usernames
   if(!is.na(userids))
     args$user_ids <- userids
-  message(paste(url,"?",names(args[1]),"=",args[1],sep=''))
+#   message(paste(url,"?",names(args[1]),"=",args[1],sep=''))
   xx <- getForm(url, 
    .params = args,
    ...,
